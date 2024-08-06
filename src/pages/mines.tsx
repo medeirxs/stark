537afcc22892571d84133111318a71a75945f81a
Matheus Medeiros <mtmedeirosgs@gmail.com>
2024-08-06 18:40:02 -0300

    feat: finish mines scheme

--- src/pages/mines.tsx
+++ src/pages/mines.tsx
@@ -1,48 +1,71 @@
-import defaultt from "@/assets/default.png"
-import diamond from "@/assets/diamond.png"
-import explosion from "@/assets/explosion.png"
+import { useEffect } from "react";
+import defaultt from "@/assets/default.png";
+import diamond from "@/assets/diamond.png";
+import explosion from "@/assets/explosion.png";
+import { Button } from "@/components/ui/button";
 
-export function Mines(){
-  const draw = document.querySelectorAll(".dr")
+export function Mines() {
+  useEffect(() => {
+    const draw = document.querySelectorAll(".dr");
 
-  draw.forEach((ob) => {
-    ob.addEventListener("click", (evt) => {
-      const el = evt.target
-      let RandomNumber = Math.floor(Math.random() * 10 + 1)
-      let WinNumber = Math.floor(Math.random() * 10 + 1)
-      if(RandomNumber === WinNumber){
-        el.src = diamond
-      }else{
-        el.src = explosion
-      }
+    const resetGame = () => {
+      draw.forEach((el) => {
+        el.src = defaultt
+        el.style.pointerEvents = "auto"
+      });
+    };
+
+    draw.forEach((ob) => {
+      ob.addEventListener("click", (evt) => {
+        const el = evt.target;
+        let DrawNumber = Math.floor(Math.random() * 10 + 1)
+        let WinNumber = Math.floor(Math.random() * 10)
+        if(DrawNumber >= WinNumber){
+          el.src = diamond
+        }else{
+          el.src = explosion
+          draw.forEach((el) => {
+            el.style.pointerEvents = "none"
+          });
+          setTimeout(() => {
+            resetGame()
+          }, 4000)
+        }
+      });
     });
-  });
 
-  return(
+    return () => {
+      draw.forEach((ob) => {
+        ob.removeEventListener("click", () => {});
+      });
+    };
+  }, []);
+
+  return (
     <div className="mx-auto bg-red-500 w-[360px] p-3">
       <h1 className="text-white">mines</h1>
       <div className="grid grid-cols-4 gap-1">
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
-        <img src={defaultt} className="w-20 dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
+        <img src={defaultt} className="w-20 cursor-pointer dr" />
       </div>
     </div>
-  )
+  );
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,12 @@
 import { createBrowserRouter } from "react-router-dom"
 
-import { Homepage } from "@/pages/homepage"
 import { Mines } from "@/pages/mines"
 
 export const r = createBrowserRouter([
   {
     path: '/',
     children: [
-      { path: '/', element: <Homepage /> },
-      { path: '/mines', element: <Mines /> },
+      { path: '/', element: <Mines /> },
     ],
   },
 ])
